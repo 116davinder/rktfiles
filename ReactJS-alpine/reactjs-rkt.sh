973181fa01aafdeb921f972320fada347bc8e628
Davinder Pal <dpsangwal@gmail.com>
2017-06-12 15:38:50 +0530

    added reactjs based rkt container image

---
+++ ReactJS-alpine/reactjs-rkt.sh
@@ -0,0 +1,46 @@
+#!/usr/bin/env bash
+set -e
+
+if [ "$EUID" -ne 0 ]; then
+    echo "This script uses functionality which requires root privileges"
+    exit 1
+fi
+
+# Start the build with an empty ACI
+acbuild --debug begin
+
+# Adding Maintainer Information
+acbuild label add NAME "DAVINDER PAL"
+acbuild label add EMAIL "dpsangwal@gmail.com"
+
+# In the event of the script exiting, end the build
+trap '{ export EXT=$?; acbuild --debug end && exit $EXT; }' EXIT
+
+# Name the ACI Image
+acbuild --debug set-name 116daivnder/reactjs
+
+# Based on alpine
+acbuild --debug dep add quay.io/coreos/alpine-sh
+
+# Install nodejs
+acbuild --debug run -- apk update
+acbuild --debug run -- apk add nodejs
+acbuild --debug run -- mkdir -p /app
+
+# Add a port for http traffic on port 7001
+acbuild --debug port add http tcp 7001
+
+# Copy the application to the image
+acbuild --debug copy-to-dir copy ./* /app
+
+# Setting Working Directory
+acbuild set-working-directory /app
+
+# Installing Dependenecies
+acbuild --debug run -- npm install
+
+# Run nodejs with the app
+acbuild --debug set-exec -- /usr/bin/npm start
+
+# Write the result
+acbuild --debug write --overwrite reactjs-alpine.aci
